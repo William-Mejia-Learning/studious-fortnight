9afc2e5ed8044a72f66b6b9032f06e69264e64a6
williamxmejia <willx1995@gmail.com>
2022-09-05 10:08:06 -0500

    Repeated Tests

--- src/test/java/com/healthycoderapp/DietPlannerTest.java
+++ src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -2,6 +2,7 @@ package com.healthycoderapp;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +20,7 @@ class DietPlannerTest {
         System.out.println("A unit test was finished");
     }
 
-    @Test
+    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
     void should_ReturnCorrectDietPlan_When_CorrectCoder() {
 
         //given
